e06157a4301af14c0f2f48354b93d3f5865b5d09
mgore <manojgore.mg@gmail.com>
2024-09-07 12:32:17 +0530

    cvx

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ arr.forEach(button => {
         if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
+            return;
         }
 
-        string += e.target.innerHTML;
-        input.value = string;
-    })
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+            return;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    });
 });
