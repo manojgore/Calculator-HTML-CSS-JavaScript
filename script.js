f4f1aa01908996b478fc2c51fee48a3f93761cc6
mgore <manojgore.mg@gmail.com>
2024-09-07 12:40:49 +0530

    done

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button,addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-            return;
-        }
 
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-            return;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-        
-    });
+buttons.forEach(button => {
+  button.addEventListener('click', (e)   => {
+    if (e.target.innerHTML == '=') {
+      try {
+        string = eval(string);
+      } catch (error) {
+        console.error("Invalid   expression:", error);
+        string = "";
+      }
+    } 
+    
+    else if (e.target.innerHTML == 'AC') {
+      string = "";
+    } 
+    else if(e.target.innerHTML == 'DEL') {
+        string = string.substring(0, string.length-1);
+        input.value = string;
+    }
+    else {
+      string += e.target.innerHTML;
+    }
+    input.value = string;
+  });
 });
